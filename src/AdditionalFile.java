848cc1e2ad419f2dab1175a7c143f127f92a10e0
Ipaye Babatunde <fineboyhakim@gmail.com>
2024-02-22 23:57:51 +0100

    my changes

---
+++ src/AdditionalFile.java
@@ -0,0 +1,19 @@
+public class AdditionalFile {
+    public static void main(String[] args) {
+       int num1;
+       int num2;
+       int num3;
+       int r;
+
+       num1 = 100;
+       num2 = 10;
+       num3 = 29;
+
+       r = num1 + num2 * num3;
+
+       // where r represent the Result
+        System.out.println(r);
+
+
+    }
+}
